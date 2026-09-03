7004412a19b0638833dab0d4bced7598a3c93702
Elan Wittevrongel <elan.wittevrongel@gmail.com>
2026-09-03 12:03:44 +0200

    wrong username, password + empty credentials

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,6 +5,7 @@ export class LoginPage extends BasePage {
     readonly usernameInput: Locator = this.page.getByRole('textbox', { name: 'Username' });
     readonly passwordInput: Locator = this.page.getByRole('textbox', { name: 'Password' });
     readonly loginButton: Locator = this.page.getByRole('button', { name: 'Login' });
+    readonly errorMessage: Locator = this.page.locator('[data-test="error"]');
 
     async goto(): Promise<void> {
         await this.page.goto('/');
@@ -18,5 +19,12 @@ export class LoginPage extends BasePage {
 
     async loginSucces(): Promise<void> {
         await expect(this.page).toHaveURL(/inventory/);
+    }
+
+    async loginFailure(expectedMessage?: string): Promise<void> {
+        await expect(this.errorMessage).toBeVisible();
+        if (expectedMessage) {
+            await expect(this.errorMessage).toContainText(expectedMessage);
+        }
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,21 @@
 import { test, expect } from '../fixtures/fixtures';
-import { LoginPage } from '../pages/LoginPage';
 
 test.describe('Login', () => {
     test('user can log in with valid credentials', async ({ loginPage }) => {
         await loginPage.goto();
         await loginPage.login('standard_user', 'secret_sauce');
+        await loginPage.loginSucces();
     });
+
+    test('wrong username and password shows specific error', async ({ loginPage }) => {
+    await loginPage.goto();
+    await loginPage.login('wrong_user', 'wrong_password');
+    await loginPage.loginFailure('Epic sadface: Username and password do not match any user in this service');
+    });
+
+    test('empty credentials shows an error', async ({ loginPage }) => {
+    await loginPage.goto();
+    await loginPage.login('', '');
+    await loginPage.loginFailure('Epic sadface: Username is required');
+});
 });
